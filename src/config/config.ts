cfa5ca68da1036dce175affa7751a223cd9196d2
Mike Eling <mike.eling97@gmail.com>
2020-04-10 21:38:32 +0200

    :pencil2: Update server_url

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,3 +1,3 @@
 export default {
-    server_url: "http://localhost:5002/strokey-8bc36/us-central1/"
+    server_url: "https://us-central1-strokey-8bc36.cloudfunctions.net/" //"http://localhost:5002/strokey-8bc36/us-central1/"
 }
